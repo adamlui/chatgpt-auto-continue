473f275d2e8d230835f46211b9197d9d175b53a3
adamlui@protonmail.com <adamlui@protonmail.com>
2024-02-07 09:15:04 -0800

    Moved plugin `extends` into `overrides`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'extends': ['eslint:recommended', 'plugin:json-schema-validator/recommended', 'plugin:yml/standard'],
+    'extends': ['eslint:recommended'],
     'ignorePatterns': ['!/.github'],
     'rules': {
         'indent': 'off', 'no-unexpected-multiline': 'off', // allow whitespace anywhere
@@ -13,5 +13,9 @@ module.exports = {
     },
     'globals': { 'chatgpt': 'readonly' },
     'parserOptions': { 'ecmaVersion': 2022, 'sourceType': 'script' },
+    'overrides': [
+        { 'files': ['**/*.json'], 'extends': ['plugin:json-schema-validator/recommended'] },
+        { 'files': ['**/*.yml'], 'extends': ['plugin:yml/standard'] }
+    ],
     'env': { 'browser': true, 'node': true, 'es6': true, 'greasemonkey': true }
 };
