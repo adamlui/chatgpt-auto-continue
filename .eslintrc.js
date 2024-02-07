906042563716913cc3b286c300e59393f9d48db1
adamlui@protonmail.com <adamlui@protonmail.com>
2024-02-07 07:42:45 -0800

    Added `'no-unexpected-multiline': 'off',` to `ruless`
    ... to allow whitespace anywhere

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     'extends': ['eslint:recommended', 'plugin:json-schema-validator/recommended'],
     'rules': {
-        'indent': 'off',
+        'indent': 'off', 'no-unexpected-multiline': 'off', // allow whitespace anywhere
         'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }], // enforce single quotes for string literals
         'key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }], // enforce spacing in object properties
         'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
